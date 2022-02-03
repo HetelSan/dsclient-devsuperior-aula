50da83fa451d814b2488973bd20de213d8b39dab
Hetel Santos <hetel.santos@gmail.com>
2022-02-03 08:08:06 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dsclient.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repositories.ClientRepository;
 
@@ -16,8 +18,15 @@ public class ClientService {
 	private ClientRepository clientRepository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return clientRepository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = clientRepository.findAll();
+		
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		
+		//List<ClientDTO> listDTO = new ArrayList<>();
+		//for (Client cli : list) {
+		//	listDTO.add(new ClientDTO(cli));
+		//}
 	}
 
 }
