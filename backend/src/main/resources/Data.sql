a0636c88155b184474aa24745f3cd35887f6a986
Hetel Santos <hetel.santos@gmail.com>
2022-02-02 22:29:24 -0300

    Database seeding

---
+++ backend/src/main/resources/Data.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Hetel Santos', '28555282080', TIMESTAMP WITH TIME ZONE '1962-08-27T06:36:00.12345Z', 5000.0, 1);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Simone Diniz', '22642207080', TIMESTAMP WITH TIME ZONE '1965-10-10T09:45:00.12345Z', 15000.0, 1);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Gustavo Santos Diniz', '79717279098', TIMESTAMP WITH TIME ZONE '1996-09-06T07:00:00.12345Z', 1000.0, 0);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Maria da Montanha Diniz', '88562948004', TIMESTAMP WITH TIME ZONE '1942-11-02T19:30:00.12345Z', 3000.0, 3);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Heleno Santos', '49066129034', TIMESTAMP WITH TIME ZONE '1961-05-30T16:00:00.12345Z', 2000.0, 0);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Patricia dos Santos', '42303040051', TIMESTAMP WITH TIME ZONE '1982-04-25T15:00:00.12345Z', 2500.0, 1);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Heder dos Santos', '34339539023', TIMESTAMP WITH TIME ZONE '1986-08-20T12:00:00.12345Z', 7000.0, 1);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Claudio Diniz', '11355583020', TIMESTAMP WITH TIME ZONE '1970-07-20T18:50:00.12345Z', 4000.0, 2);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Jenilso Felix da Silva', '70165781009', TIMESTAMP WITH TIME ZONE '1960-01-06T02:30:00.12345Z', 3800.0, 2);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('José João de Souza', '90524561001', TIMESTAMP WITH TIME ZONE '1950-01-01T20:30:00.12345Z', 3900.0, 6);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Pedro Paulo da Silva', '07689739024', TIMESTAMP WITH TIME ZONE '1980-12-31T23:30:00.12345Z', 2900.0, 4);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Fernando Fernandez', '87017249089', TIMESTAMP WITH TIME ZONE '1990-06-15T22:30:00.12345Z', 3500.0, 3);
+INSERT INTO tb_client (name, cpf, birth_date, income, children) VALUES ('Osvaldo Spina', '55128724000', TIMESTAMP WITH TIME ZONE '1991-04-03T21:00:00.12345Z', 12200.0, 2);
